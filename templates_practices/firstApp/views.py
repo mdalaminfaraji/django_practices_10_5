9b35841cdf61d51bf0e4c07e65ec58e1374f6323
mdalaminfaraji <alaminice1617@gmail.com>
2023-12-01 17:53:22 +0600

    set some tag

--- templates_practices/firstApp/views.py
+++ templates_practices/firstApp/views.py
@@ -6,5 +6,9 @@ def home(request):
          return HttpResponse("Hello, world. You're at the polls index.")
 
 def index(request):
-        context={'a':1, 'b':2}
+        context={'a':1, 'b':2,  'genres':['Fiction', 'Non-Fiction', 'Science Fiction', 'Mystery'],   'special_book' :{
+        'title': 'The Special Book',
+        'author': 'Special Author',
+        'publication_year': 2022,
+    }}
         return render(request, 'index.html', context)
